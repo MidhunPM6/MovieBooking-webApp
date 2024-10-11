d583a8bbaecbc452a0f99772ab988c0a5ac0cee7
MidhunPM6 <midhunpm6060@gmail.com>
2024-10-11 14:10:35 +0530

    Firebase Initialized

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBx4KrEHLX2Div8JkfJqNIHSxD7XZQ-Vvg",
+  authDomain: "moviebokingapp-8cc5a.firebaseapp.com",
+  projectId: "moviebokingapp-8cc5a",
+  storageBucket: "moviebokingapp-8cc5a.appspot.com",
+  messagingSenderId: "627440074275",
+  appId: "1:627440074275:web:9c75b953ed73d2a5813b96",
+  measurementId: "G-RTD49HQRBL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);  
+export const db = getFirestore(app);
